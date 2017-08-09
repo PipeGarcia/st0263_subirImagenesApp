d0dc812620367a2a263074f4dcf232771160c40b
Pipe <pipefutbol8@gmail.com>
2017-08-08 22:07:19 -0500

    modificación en el string de conexión a la BD

--- app.js
+++ app.js
@@ -83,7 +83,3 @@ app.set('port', (process.env.PORT || 3000));
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
 });
-
-/**app.post('/', upload.any(), function(req, res, next){
-  res.send(req.files);
-});*/
