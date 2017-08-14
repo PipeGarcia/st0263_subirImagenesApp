596640c2c95330d4707cdeed7022fd349d6a24a2
Pipe <pipefutbol8@gmail.com>
2017-08-13 23:26:12 -0500

    cambio de puerto

--- app2.js
+++ app2.js
@@ -79,7 +79,7 @@ app.use('/', routes);
 app.use('/', users);
 app.use('/', routesImages);
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3006));
 
 app.listen(app.get('port'), function(){
 	console.log('Server started on port '+app.get('port'));
